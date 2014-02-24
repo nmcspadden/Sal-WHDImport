326b791e4a2a546a57cad9b84e0378672770ab02
nmcspadden <nmcspadden@gmail.com>
2014-02-24 15:38:29 -0800

    Added hd_total, fixed memorytotal.

--- models.py
+++ models.py
@@ -7,13 +7,14 @@ class Whdmachine(models.Model):
     hostname = models.TextField()
     mac_address_en0 = models.TextField()
     productname = models.TextField()
-    memory_total = models.TextField()
+    memorytotal = models.TextField()
     cpu = models.TextField()
     macosx_productversion = models.TextField()
     ipaddress = models.TextField()
     sp_local_host_name = models.TextField()
     macaddress_wifi = models.TextField()
     macaddress_eth = models.TextField()
+    hd_total = models.TextField()
     
     def __unicode__(self):
         return self.hostname
