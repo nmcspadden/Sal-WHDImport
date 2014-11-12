b7bda47a6996281873f65dba976cdc9625cdeae8
Nick McSpadden <nmcspadden@gmail.com>
2014-11-12 15:37:39 -0800

    Allowed null for all models

--- models.py
+++ models.py
@@ -3,19 +3,19 @@ from server.models import Machine
 
 class Whdmachine(models.Model):
 	machine = models.ForeignKey(Machine)
-	serial = models.TextField()
-	hostname = models.TextField()
+	serial = models.TextField(null=True)
+	hostname = models.TextField(null=True)
 	mac_address_en0 = models.TextField(blank=True, null=True)
-	productname = models.TextField()
-	memorytotal = models.TextField()
-	cpu = models.TextField()
-	macosx_productversion = models.TextField()
-	ipaddress = models.TextField()
-	sp_local_host_name = models.TextField()
+	productname = models.TextField(null=True)
+	memorytotal = models.TextField(null=True)
+	cpu = models.TextField(null=True)
+	macosx_productversion = models.TextField(null=True)
+	ipaddress = models.TextField(null=True)
+	sp_local_host_name = models.TextField(null=True)
 	macaddress_wifi = models.TextField(blank=True, null=True)
 	macaddress_eth = models.TextField(blank=True, null=True)
-	hd_total = models.TextField()
-	type = models.TextField()
+	hd_total = models.TextField(null=True)
+	type = models.TextField(null=True)
 	
 	def __unicode__(self):
 		return self.hostname
