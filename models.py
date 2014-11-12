c11cf6dafc41185e44667e90da35d3a2a617224d
Nick McSpadden <nmcspadden@gmail.com>
2014-11-12 15:46:53 -0800

    I am bad at Django

--- models.py
+++ models.py
@@ -3,19 +3,19 @@ from server.models import Machine
 
 class Whdmachine(models.Model):
 	machine = models.ForeignKey(Machine)
-	serial = models.TextField(null=True)
-	hostname = models.TextField(null=True)
-	mac_address_en0 = models.TextField(null=True)
-	productname = models.TextField(null=True)
-	memorytotal = models.TextField(null=True)
-	cpu = models.TextField(null=True)
-	macosx_productversion = models.TextField(null=True)
-	ipaddress = models.TextField(null=True)
-	sp_local_host_name = models.TextField(null=True)
-	macaddress_wifi = models.TextField(null=True)
-	macaddress_eth = models.TextField(null=True)
-	hd_total = models.TextField(null=True)
-	type = models.TextField(null=True)
+	serial = models.TextField(blank=True, default="")
+	hostname = models.TextField(blank=True, default="")
+	mac_address_en0 = models.TextField(blank=True, default="")
+	productname = models.TextField(blank=True, default="")
+	memorytotal = models.TextField(blank=True, default="")
+	cpu = models.TextField(blank=True, default="")
+	macosx_productversion = models.TextField(blank=True, default="")
+	ipaddress = models.TextField(blank=True, default="")
+	sp_local_host_name = models.TextField(blank=True, default="")
+	macaddress_wifi = models.TextField(blank=True, default="")
+	macaddress_eth = models.TextField(blank=True, default="")
+	hd_total = models.TextField(blank=True, default="")
+	type = models.TextField(blank=True, default="")
 	
 	def __unicode__(self):
 		return self.hostname
