e982fd20fea162a594b34c655e8eccc86e9ef4e4
Nick McSpadden <nmcspadden@gmail.com>
2014-11-20 10:26:36 -0800

    Accommodated possibility of bad serial

--- management/commands/syncmachines.py
+++ management/commands/syncmachines.py
@@ -93,8 +93,12 @@ class Command(BaseCommand):
 				print whd_machine.serial + " " + fact_name + " doesn't exist."
 			if mms_available:
 				# If we have MacModelShelf, then we can pull it based on the serial number.
-				print "Model " + macmodelshelf.model(macmodelshelf.model_code(whd_machine.serial).encode())
-				whd_machine.productname = macmodelshelf.model(macmodelshelf.model_code(whd_machine.serial).encode()).encode()
+				local_model = macmodelshelf.model(macmodelshelf.model_code(whd_machine.serial).encode())
+				print local_model
+				if local_model is None:
+					whd_machine.productname = ""
+				else:
+					whd_machine.productname = macmodelshelf.model(macmodelshelf.model_code(whd_machine.serial).encode()).encode()
 			else:
 				# Otherwise, just use the productname pulled from the fact.
 				whd_machine.productname = productname
